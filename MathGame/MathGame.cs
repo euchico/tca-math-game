1824bd7682577516170367e064a62b6cf3689312
euchico <franciscodepaula.web@gmail.com>
2025-06-02 11:30:00 -0300

    feat: Adição do menu.

---
+++ MathGame/MathGame.cs
@@ -0,0 +1,19 @@
+﻿using MathGame.View;
+
+namespace MathGame;
+
+internal class MathGame
+{
+    static void Main(string[] args)
+    {
+        MainScreen screen = new MainScreen();
+        screen.Show();
+
+        Console.Write("Opção: ");
+        int operation = int.Parse(Console.ReadLine()!);
+
+        Console.Write(operation);
+
+        Console.ReadKey();
+    }
+}
---
+++ MathGame/View/MainScreen.cs
@@ -0,0 +1,18 @@
+﻿namespace MathGame.View;
+
+internal class MainScreen
+{
+    internal void Show()
+    {
+        Console.WriteLine(">>> Jogo Matemático <<<\n");
+
+        Console.WriteLine("Escolha o modo de jogo:");
+        Console.WriteLine("1. Adição");
+        Console.WriteLine("2. Subtração");
+        Console.WriteLine("3. Multiplicação");
+        Console.WriteLine("4. Divisão");
+        Console.WriteLine("5. Modo Aleatório\n");
+
+        Console.WriteLine("0. Sair\n");
+    }
+}
